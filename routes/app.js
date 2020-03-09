89a01b15848f70264928f1c1a179f6810c1470dd
Austin Dryden <austinisnorobot@gmail.com>
2020-03-08 20:17:42 -0400

    sets up /room routing.

--- routes/app.js
+++ routes/app.js
@@ -6,84 +6,14 @@ const del = require('../models/deletequery');
 const get = require('../models/getquery');
 const {JWTCheck} = require('../models/userquery');
 const userRouter = require('./user');
-
+const roomRouter = require('./room');
 
 router.use('/user', userRouter);
 
 // This is the JWT validation check. Check if token is valid, attach token payload to req.body and call next. if not, return JSON login error.
 router.use(JWTCheck);
 
-router.get('/rooms/user/', async (req, res)=>{
-    console.log(req.body);
-    let {userId} = req.body.token;
-    let allRooms = await get.allRoomsByUser(userId);
-    res.json(allRooms);
-});
-
-router.get('/plants/room/', async (req, res)=>{
-    let {plantId} = req.body;
-    let allPlants = await get.allPlantsByRoom(plantId);
-    res.json(allPlants);
-});
-
-router.post('/room', async (req, res)=>{
-    try{
-        let newRoom = req.body;
-        console.log(newRoom);
-        //function call to decode JWT into payload object goes here
-        let newRec = await post.addRoom(newRoom);
-        if (!newRec.error){
-            return res.json(newRec);
-        } 
-        res.status(404).json(newRec);
-    }catch(e){
-        console.log(e);
-        res.json({horse:"shit"})
-    }
-});
-
-router.get('/room', async (req, res)=>{
-    try{
-        let room = req.body;
-        
-        let newRec = await get.oneRoom(room.id);
-        res.json(newRec);
-    }catch(e){
-        console.log(e);
-        res.json({horse:"shit"})
-    }
-});
-
-router.put('/room', async (req, res)=>{
-    try{
-        let updateRoom = req.body;
-        
-        let updateRec = await put.updateRoom(updateRoom);
-        console.log(updateRec);    
-        if (!updateRec.error){
-            return res.json(updateRec);
-        } 
-        res.status(404).json(updateRec);
-    }catch(e){
-        console.log(e);
-        res.json({horse:"shit"})
-    }
-});
-
-router.delete('/room', async (req, res)=>{
-    try{
-        let delRoom = req.body;
-        //function call to decode JWT into payload object goes here
-        let delRec = await del.deleteRoom(delRoom.id);
-        if (!delRec.error){
-                return res.json(delRec);
-            }
-        res.status(404).json(updateRec);
-    }catch(e){
-        console.log(e);
-        res.json({horse:"shit"})
-    }
-});
+router.use('/room', roomRouter);
 
 router.post('/plantinfo', async (req, res)=>{
     try{
